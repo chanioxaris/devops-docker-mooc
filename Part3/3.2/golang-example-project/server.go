84878802b0ab44a128a430c761a6264a8b18cede
Haris Chaniotakis <chanioxaris@gmail.com>
2020-04-18 21:28:15 +0300

    Check action trigger

--- Part3/3.2/golang-example-project/server.go
+++ Part3/3.2/golang-example-project/server.go
@@ -15,7 +15,7 @@ func main() {
 }
 
 func Ping() http.HandlerFunc {
-	return func(w http.ResponseWriter, _ *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write([]byte("pong")); err != nil {
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 		}
